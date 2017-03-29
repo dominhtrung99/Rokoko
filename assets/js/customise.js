1c8fe935a8242e339a8f664a39caedaf6149d157
Do <hondaomauxanh@yahoo.com>
2017-03-30 01:53:48 +0700

    Complete đặt bàn

---
+++ assets/js/customise.js
@@ -0,0 +1,8 @@
+$(function($){
+    $('#datetimepicker1').datetimepicker({
+    	format: 'DD/MM/YYYY'
+    });
+
+     $('#timepicker1').timepicker();
+    
+});
---
+++ www/assets/js/customise.js
@@ -0,0 +1,8 @@
+$(function($){
+    $('#datetimepicker1').datetimepicker({
+    	defaultDate: "11/1/2013"
+    });
+    $('#datetimepicker2').datetimepicker({
+    	defaultDate: "11/1/2013"
+    });
+});
