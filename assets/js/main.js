a8d31d6aaeb7a82e8ae10571ee135b5d84ec8636
Bui Phu Quoc <bpquoc91@gmail.com>
2017-04-01 21:59:53 +0700

    update responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,20 @@ jQuery(function($){
         $(".menu-item").on("click", function(){
             $(".menu-item").removeClass("active");
             $(this).addClass("active");
-        })        
+        })
+
+        $(".wrapper-hamburger").on("click", function(e){
+            e.stopPropagation();
+            $("body").toggleClass("show-body");
+        })
+
+        $("body").on("click", function(){
+            $(this).removeClass("show-body");
+        })
+
+        $(".menu-mobile").on("click", function(e){
+            e.stopPropagation();
+        })    
     };
 
     /*=======================================
