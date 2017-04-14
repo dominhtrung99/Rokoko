f7a053ad3e16a5c1834566c4ac11bb7606b7872a
Do <hondaomauxanh@yahoo.com>
2017-04-14 14:13:25 +0700

    Update feedback 27 28 29 31 32 33 34

--- www/assets/js/customise.js
+++ www/assets/js/customise.js
@@ -21,12 +21,50 @@ $(function($){
 	    $('#timepicker1').timepicker();
  	}
 
+ 	
+	  $(".album-carousel").owlCarousel({
+	  		items: 1,
+	  		loop:true,
+	  		responsive:{
+	  			0:{
+		            items:1
+		        },
+		        600:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+	  		}
+	  });
+
+	  
+
 	if($('.space-wrapper').length > 0) {
+
+
+		var owl = $('.album-carousel');
+
+		$('.rokoko-prev-btn').click(function() {
+		    owl.trigger('prev.owl.carousel');
+		})
+		// Go to the previous item
+		$('.rokoko-next-btn').click(function() {
+		    // With optional speed parameter
+		    // Parameters has to be in square bracket '[]'
+		    owl.trigger('next.owl.carousel', [300]);
+		})
+
+
 		$(".btn-detail").on( "click", function(e) {
 			e.preventDefault();
-			
-			var target = $(this).closest(".item-gird-wr");
-			showItemPopup(target);
+			var target = $(this).attr("href");
+			owl.trigger('refresh.owl.carousel');
+			$(target).modal();
+
+
+			//showItemPopup(target);
+
 			// var index = $( ".item-gird-wr" ).index( target );
 			// //console.log('index',index);
 			// $("#current_index_popup").val(index);
@@ -37,44 +75,46 @@ $(function($){
 			// $("#title_model").html(target.find(".title").html());
 			// $("#date_model").html(target.find(".date").html());
 			// $("#des_model").html(target.find(".description").html());
-		  $('#spaceModel').modal();
+		  //$('#spaceModel').modal();
 
 		  return false;
 		});
 
-		$(".rokoko-next-btn").on( "click", function(e) {
-			e.preventDefault();
-			var current = parseInt($("#current_index_popup").val());
-
-			var len = $( ".item-gird-wr" ).length;
-			//console.log(len);
-			
-			var index = 0;
-			if(current < len - 1){
-				index = current + 1;
-			}
-			var target = $(".item-gird-wr").eq(index);
-			showItemPopup(target);
-			 return false;
-		});
-
-		$(".rokoko-prev-btn").on( "click", function(e) {
-			e.preventDefault();
-			var current = parseInt($("#current_index_popup").val());
-
-			var len = $( ".item-gird-wr" ).length;
-			var index = 0;
-			if(current === 0){
-				index = len - 1;
-			} else {
-				index = current - 1;
-			}
-			var target = $(".item-gird-wr").eq(index);
-			showItemPopup(target);
-			 return false;
-		});
-		
 	}
+
+	// 	$(".rokoko-next-btn").on( "click", function(e) {
+	// 		e.preventDefault();
+	// 		var current = parseInt($("#current_index_popup").val());
+
+	// 		var len = $( ".item-gird-wr" ).length;
+	// 		//console.log(len);
+			
+	// 		var index = 0;
+	// 		if(current < len - 1){
+	// 			index = current + 1;
+	// 		}
+	// 		var target = $(".item-gird-wr").eq(index);
+	// 		//showItemPopup(target);
+	// 		 return false;
+	// 	});
+
+	// 	$(".rokoko-prev-btn").on( "click", function(e) {
+	// 		e.preventDefault();
+	// 		var current = parseInt($("#current_index_popup").val());
+
+	// 		var len = $( ".item-gird-wr" ).length;
+	// 		var index = 0;
+	// 		if(current === 0){
+	// 			index = len - 1;
+	// 		} else {
+	// 			index = current - 1;
+	// 		}
+	// 		var target = $(".item-gird-wr").eq(index);
+	// 		showItemPopup(target);
+	// 		 return false;
+	// 	});
+		
+	// }
       
 });
 
